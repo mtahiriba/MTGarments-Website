c80f0e013d35f762bd6051013deabd6f8dee4716
Lareb-Malik <larebm80@gmail.com>
2022-08-13 03:37:54 -0700

    Product description and details are added and linked with product page

--- project/JS/cards.js
+++ project/JS/cards.js
@@ -4,49 +4,124 @@ womencardlist = [
         text: 'Silver Dress',
         price: 7999,
         image1: './images/first1.jpg',
-        image2: './images/first2.jpg'
+        description1:`Length: 74cm
+        Regular fit
+        Notched lapels
+        Twin button front fastening
+        Front patch pockets; chest pocket
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandAlessi
+        Reference: PD0023
+        In stock 300 Items`,
+        stock:300
     },
     {
         id: '2',
         text: 'Pent Shirt',
         price: 6999,
         image1: './images/second1.jpg',
-        image2: './images/second2.jpg'
+        description1:`Length: 80cm
+        Regular fit
+        Beautiful neck
+        With Jeans
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandEva-Solo
+        Reference: PD0022
+        In stock 420 Items`
     },
     {
         id: '3',
         text: 'Black Dress',
         price: 8999,
         image1: './images/third1.jpg',
-        image2: './images/third2.jpg'
+        description1:`Length: 74cm
+        Regular fit
+        Notched lapels
+        Twin button front fastening
+        Front patch pockets; chest pocket
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandHay
+        ReferencePD0024
+        In stock 250 Items`
     },
     {
         id: '4',
         text: 'Blue Pents',
         price: 4999,
         image1: './images/four1.jpg',
-        image2: './images/four2.jpg'
+        description1:`Length: 80cm
+        Regular fit
+        Beautiful neck
+        With Jeans
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandVitra
+        ReferencePD0028
+        In stock 300 Items`
     },
     {
         id: '5',
         text: 'Red T-Shirt',
         price: 8599,
         image1: './images/five1.jpg',
-        image2: './images/five2.jpg'
+        description1:`Length: 74cm
+        Regular fit
+        Notched lapels
+        Twin button front fastening
+        Front patch pockets; chest pocket
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandRosenthal
+        ReferencePD0029
+        In stock 200 Items`
     },
     {
         id: '6',
         text: 'White Dress',
         price: 4499,
         image1: './images/six1.jpg',
-        image2: './images/six2.jpg'
+        description1:`Length: 80cm
+        Regular fit
+        Beautiful neck
+        With Jeans
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandRosenthal
+        ReferencePD0029
+        In stock 200 Items`
     },
     {
         id: '7',
         text: 'New Brown Dress',
         price: 7799,
         image1: './images/seven1.jpg',
-        image2: './images/seven2.jpg'
+        description1:`Length: 74cm
+        Regular fit
+        Notched lapels
+        Twin button front fastening
+        Front patch pockets; chest pocket
+        Internal pockets
+        Centre-back vent
+
+        Material: Outer: 50% Linen & 50% Polyamide; Body Lining: 100% Cotton; Lining: 100% Acetate`,
+        productDetail:`BrandPackIt
+        ReferencePD0031
+        In stock 300 Items`
     }
 ]
 
@@ -56,28 +131,56 @@ mencardlist = [
         text: 'Gray Suit',
         price: 14999,
         image1: './images/menfirst.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Blended
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD00100
+        In stock 500 Items`
     },
     {
         id: '2',
         text: 'Formal Pant',
         price: 2999,
         image1: './images/mensecond.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0031
+        In stock 100 Items`
     },
     {
         id: '3',
         text: 'Black Shirt',
         price: 3999,
         image1: './images/menthird.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD00314
+        In stock 170 Items`
     },
     {
         id: '4',
         text: 'Black Trouser',
         price: 5299,
         image1: './images/mensix.jpg',
-        image2: './images/first2.jpg'
+        description1:`Mens Jeans from Ideas are the perfect fit for a smart-casual everyday look.
+
+        98 % Cotton 2 % lycra
+        Slim Fit
+        Model Specs: The model is 6ft and is wearing size 30.`,
+        productDetail:`BrandPackIt
+        Reference: PD0061
+        In stock 10 Items`
     }
     ,
     {
@@ -85,14 +188,28 @@ mencardlist = [
         text: 'Blue Coat',
         price: 10999,
         image1: './images/menfive.png',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Blended
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD00771
+        In stock 155 Items`
     },
     {
         id: '6',
         text: 'Formal Shirt',
         price: 10000,
         image1: './images/menfour.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0931
+        In stock 100 Items`
     }
 ]
 
@@ -103,14 +220,28 @@ kidscardlist = [
         text: 'Pink Dress',
         price: 7999,
         image1: './images/kids1.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0921
+        In stock 332 Items`
     },
     {
         id: '2',
         text: 'New Black dress',
         price: 4499,
         image1: './images/kids7.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD088
+        In stock 443 Items`
     }
     ,
     {
@@ -118,56 +249,112 @@ kidscardlist = [
         text: 'Blue suit',
         price: 5999,
         image1: './images/kids3.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0444
+        In stock 66 Items`
     },
     {
         id: '4',
         text: 'New Dress',
         price: 5399,
         image1: './images/kids4.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0111
+        In stock 100 Items`
     },
     {
         id: '5',
         text: 'Formal Dress',
         price: 6999,
         image1: './images/kids5.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0110
+        In stock 110 Items`
     },
     {
         id: '6',
         text: 'Formal Dress',
         price: 3299,
         image1: './images/kids6.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0101
+        In stock 82 Items`
     },
     {
         id: '7',
         text: 'Formal Dress',
         price: 3799,
         image1: './images/kids8.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0100
+        In stock 60 Items`
     },
     {
         id: '8',
         text: 'Formal Dress',
         price: 8999,
         image1: './images/kids2.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0011
+        In stock 30 Items`
     },
     {
         id: '9',
         text: 'Formal Dress',
         price: 5899,
         image1: './images/kids9.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0010
+        In stock 100 Items`
     },
     {
         id: '10',
         text: 'Formal Dress',
         price: 4999,
         image1: './images/kids10.jpg',
-        image2: './images/first2.jpg'
+        description1:`Created with premium quality material, Casual Shirts from Ideas Man are perfect for every occasion.
+
+        Cotton
+        Regular Fit
+        Model Specs: The model is 6’2 and is wearing size Medium`,
+        productDetail:`BrandPackIt
+        Reference: PD0001
+        In stock 100 Items`
     }
     
 ]
@@ -223,6 +410,8 @@ function kids(){
                     localStorage.setItem("text", kidscardlist[j].text)
                     localStorage.setItem("image", kidscardlist[j].image1)
                     localStorage.setItem("price", kidscardlist[j].price)
+                    localStorage.setItem("description", kidscardlist[j].description1)
+                    localStorage.setItem("product", kidscardlist[j].productDetail)
                 }
             }
             window.location = './product.html'
@@ -284,6 +473,8 @@ function men(){
                     localStorage.setItem("text", mencardlist[j].text)
                     localStorage.setItem("image", mencardlist[j].image1)
                     localStorage.setItem("price", mencardlist[j].price)
+                    localStorage.setItem("description", mencardlist[j].description1)
+                    localStorage.setItem("product", mencardlist[j].productDetail)
                 }
             }
             window.location = './product.html'
@@ -345,6 +536,9 @@ function women(){
                     localStorage.setItem("text", womencardlist[j].text)
                     localStorage.setItem("image", womencardlist[j].image1)
                     localStorage.setItem("price", womencardlist[j].price)
+                    localStorage.setItem("description", womencardlist[j].description1)
+                    localStorage.setItem("product", womencardlist[j].productDetail)
+
                 }
             }
             window.location = './product.html'
--- project/JS/product.js
+++ project/JS/product.js
@@ -2,16 +2,23 @@
 text = localStorage.getItem('text')
 image = localStorage.getItem('image')
 price = localStorage.getItem('price')
+descriptions = localStorage.getItem('description')
+productDetails = localStorage.getItem('product')
+
 
 btn_buy = document.getElementById('buy-now')
 document.getElementById('name1').innerText=`${text}`;
 document.getElementById('name2').innerText=`${text}`;
+document.getElementById('description').innerText=`${descriptions}`;
+document.getElementById('productDetail1').innerText=`${productDetails}`;
 document.getElementById('price1').innerText=`Rs: ${price}`;
-document.getElementById('image1').src=`${image}`
+document.getElementById('image1').src=`${image}`;
+
 btn_buy.addEventListener('click', function(){
     alert(`${text}
     ${image}
-    ${price}`)
+    ${price}
+    ${productDetails}`)
 
 })
 
