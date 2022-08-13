acf2c0a8e5d993bd613a7af76cbb36368cd323ba
Lareb-Malik <larebm80@gmail.com>
2022-08-13 01:34:55 -0700

    Product data is attachted to product page using local storage

--- project/JS/product.js
+++ project/JS/product.js
@@ -4,8 +4,14 @@ image = localStorage.getItem('image')
 price = localStorage.getItem('price')
 
 btn_buy = document.getElementById('buy-now')
+document.getElementById('name1').innerText=`${text}`;
+document.getElementById('name2').innerText=`${text}`;
+document.getElementById('price1').innerText=`Rs: ${price}`;
+document.getElementById('image1').src=`${image}`
 btn_buy.addEventListener('click', function(){
     alert(`${text}
     ${image}
     ${price}`)
+
 })
+
